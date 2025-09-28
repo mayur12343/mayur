1d7476a8bd0a7bd356714d4c5417663fdba3697a
mayur12343 <mraswale@gmail.com>
2025-09-28 10:52:53 +0530

    new

---
+++ cy.py
@@ -0,0 +1,14 @@
+
+def check_number(ma):
+    if ma > 10:
+        print("Number is greater than 10")
+    else:
+        print("Number is 10 or less")
+ma=5
+check_number(ma)
+ma=44
+if ma > 10:
+    print("Number is greater than 10")
+else:
+    print("Number is 10 or less")   
+
